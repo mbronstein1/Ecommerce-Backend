439012c7fafb11e79c8ce894b468c21dcf8fcd21
Matthew Bronstein <mbronstein1@gmail.com>
2022-10-06 20:59:54 -0500

    changed product model to include .float

--- models/Product.js
+++ models/Product.js
@@ -20,10 +20,10 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.FLOAT,
       allowNull: false,
       validate: {
-        isDecimal: true
+        isFloat: true
       }
     },
     stock: {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,10 +3,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+// find all products
+router.get('/', async (req, res) => {
+  try {
+    const getProducts = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }]
+    });
+    res.status(200).json(getProducts)
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 // get one product
